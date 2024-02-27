607e3806143d1b3a399518e8fd8cc5ca0bfee12f
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-26 21:26:49 -0600

    Update and rename ecs.tf to eks.tf

---
+++ eks.tf
@@ -0,0 +1,60 @@
+resource "aws_eks_cluster" "eks" {
+  # Name of the cluster.
+  name = "eks"
+
+  # The Amazon Resource Name (ARN) of the IAM role that provides permissions for 
+  # the Kubernetes control plane to make calls to AWS API operations on your behalf
+  role_arn = aws_iam_role.example.arn
+
+  # Desired Kubernetes master version
+  version = "1.27"
+
+  vpc_config {
+    # Indicates whether or not the Amazon EKS private API server endpoint is enabled
+    endpoint_private_access = true
+
+    # Indicates whether or not the Amazon EKS public API server endpoint is enabled
+    endpoint_public_access = true
+
+    # Must be in at least two different availability zones
+    subnet_ids = [
+      aws_subnet.public_subnet[1].id,
+      aws_subnet.public_subnet[2].id,
+      aws_subnet.private_subnet[1].id,
+      aws_subnet.private_subnet[2].id
+    ]
+  }
+
+  # Ensure that IAM Role permissions are created before and deleted after EKS Cluster handling.
+  # Otherwise, EKS will not be able to properly delete EKS managed EC2 infrastructure such as Security Groups.
+  depends_on = [
+    aws_iam_role_policy_attachment.example-AmazonEKSClusterPolicy
+  ]
+}
+
+resource "aws_eks_addon" "cni" {
+  cluster_name = aws_eks_cluster.eks.name
+  addon_name   = "vpc-cni"
+}
+
+resource "aws_eks_addon" "csi" {
+  cluster_name = aws_eks_cluster.eks.name
+  addon_name   = "aws-ebs-csi-driver"
+}
+
+data "aws_eks_cluster_auth" "eks_auth" {
+  name = "eks_auth"
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.eks.endpoint
+}
+
+output "kubeconfig" {
+  value = module.eks-kubeconfig.kubeconfig
+  sensitive = true
+}
+
+#output "kubeconfig-certificate-authority-data" {
+#  value = aws_eks_cluster.eks.certificate_authority[0].data
+#}
