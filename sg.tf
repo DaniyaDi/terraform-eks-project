a01ae062cc18947d5cd621a1c28354b6f8f41fcf
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-26 21:28:25 -0600

    Update sg.tf

--- sg.tf
+++ sg.tf
@@ -1,35 +1,20 @@
-# Define Security Group
-resource "aws_security_group" "ecs_sg" {
-  name        = "ecs-sg"
-  description = "Security group for ECS service"
-  vpc_id      = aws_vpc.main.id # Specify your VPC ID
-
-  tags = {
-    Name = "project-ecs-sg"
-  }
-
-  # Inbound rules
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
+resource "aws_security_group" "sg" {
+  name   = "sg"
+  vpc_id = aws_vpc.main.id
 
   ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 
-  # Outbound rules
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+      from_port   = 0
+      to_port     = 65535
+      protocol    = "tcp"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
 }
 
 resource "aws_security_group" "alb_sg" {
@@ -59,6 +44,6 @@ resource "aws_security_group" "alb_sg" {
     cidr_blocks = ["0.0.0.0/0"] # Allow outbound traffic to anywhere
   }
   tags = {
-    Name = "project-alb-sg"
+    Name = "eks-alb-sg"
   }
 }
