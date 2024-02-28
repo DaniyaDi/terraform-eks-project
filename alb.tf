ed9ad0e750b7cbf886605a0e6556d39885fd0e73
guldzh <145409788+guldzh@users.noreply.github.com>
2024-02-27 18:08:07 -0600

    Update alb.tf

--- alb.tf
+++ alb.tf
@@ -1,13 +1,13 @@
-#module "lb_role" {
- # source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+module "lb_role" {
+  source    = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
 
-  #role_name = "eks_lb"
-  #attach_load_balancer_controller_policy = true
+  role_name = "eks_lb"
+  attach_load_balancer_controller_policy = true
 
-  #oidc_providers = {
-   # main = {
-    #  provider_arn               = "arn:aws:iam::012345678901:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/5C54DDF35ER19312844C7333374CC09D"
-     # namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
-    #}
-  #}
-#}
+  oidc_providers = {
+    main = {
+      provider_arn               = "arn:aws:iam::012345678901:oidc-provider/oidc.eks.us-east-1.amazonaws.com/id/5C54DDF35ER19312844C7333374CC09D"
+      namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+    }
+  }
+}
