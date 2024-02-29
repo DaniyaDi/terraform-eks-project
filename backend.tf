6464cf4690f3c9ccd60352a625dbba2e5c13e319
DaniyaDi <145853610+DaniyaDi@users.noreply.github.com>
2024-02-29 15:35:03 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 #Change to your bucket name 
 terraform {
   backend "s3" {
-    bucket = "guliya-ecs-bucket"
+    bucket = "mytaskbucketdana2"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
